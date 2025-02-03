063bc54d512b1f2b72582af988d887e8f3d546a6
Delaney Blaszinski <Dablasz6@gmail.com>
2025-02-03 09:38:19 -0600

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Delaney Blaszinski on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
